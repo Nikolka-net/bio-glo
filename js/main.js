ad4b81c5d208b9a676464f5ce5968cf830034001
Nikolay <nikolka-net@mail.ru>
2019-12-06 17:42:58 +0600

    Отправка данных с mainform и captureform с по-ю ajax; вывод сообщений о статусе отправки;в поле с телефоном вводятся только цифры, в поле с именем только кириллица

--- js/main.js
+++ js/main.js
@@ -22,7 +22,6 @@ const popupCall = () => {
         popupInterval = requestAnimationFrame(popupOpacity);
         count++;
         if (count <= 100) {//уменьшаем прозрачность
-          console.log('count: ', count);
           popupContent.style.opacity = count + '%';
         } else {
           cancelAnimationFrame(popupInterval);
@@ -46,7 +45,6 @@ const popupCall = () => {
       countPopupNone();
     } else {
       target = target.closest('.popup-content');
-      console.log('target: ', target);
       if (!target) {//если не получили popup-content, т.е. получили null при клике за пределами окна
         countPopupNone();//окно исчезает при клике за пределы окна
       }
@@ -58,3 +56,131 @@ const popupCall = () => {
 };
 
 popupCall();
+
+//Send form
+
+const sendForm = () => {
+  const successMessage = 'Сообщение отправлено',
+    loadMessage = 'Идёт отправка',
+    errorMessage = 'Что-то пошло не так...';
+
+  const mainForm = document.querySelector('.main-form'),
+    captureForm = document.querySelector('.capture-form'),
+    input = document.querySelectorAll('input');
+
+  const regexp = () => {//ввод. в инпут только цифры и кириллица
+
+    input.forEach((elem) => {
+      elem.addEventListener('input', () => {
+        if (elem.name === 'user_name') {
+          elem.value = elem.value.replace(/[^а-яё\s]/ig, '');
+        }
+
+        if (elem.name === 'user_phone') {
+          elem.value = elem.value.replace(/\D/, '');
+        }
+
+      });
+
+    });
+  };
+  regexp();
+
+  const statusMessage = document.createElement('div');
+  statusMessage.style.cssText = `font-size: 2rem; color: ##90c406`;
+
+  const getForm = (event, form) => {
+    event.preventDefault();//чтобы страница не перезагружалась по умолчанию
+    form.appendChild(statusMessage);
+    statusMessage.textContent = loadMessage;//идёт загрузка
+    let formData = new FormData(form);//получ. данные нашей формы c атрибутом name в объект
+
+    let body = {};//объект, ко-й отправл. на сервер в формате json
+
+    formData.forEach((val, key) => {
+      body[key] = val;
+    });
+
+    postData(body)
+
+      .then((response) => {//данные, ко-е мы получаем
+        if (response.status !== 200) {
+          throw new Error('status network not 200');//обрабатываем как ошибку через конструктор
+        }
+        statusMessage.textContent = successMessage;
+      })
+      .catch((error) => {
+        statusMessage.textContent = errorMessage;
+        console.error(error);
+      });
+  };
+
+  const inputReset = (form) => {
+    setTimeout(() => {//очистка сообщений
+      form.removeChild(statusMessage);
+    }, 3000);
+    for (const elem of form.elements) {//очистка инпутов
+      if (elem.tagName.toLowerCase() !== 'button' && elem.type !== 'button') {
+        elem.value = '';
+      }
+    }
+  };
+
+  function valid(event, form) {
+    const elementsForm = [];//пустой массив для инпутов
+    const error = new Set();//массив для ошибочных инпутов, вмещает уникальные эл., не повторяются
+
+    for (const elem of form.elements) {//вытаскиваем из формы инпуты
+      if (elem.tagName.toLowerCase() !== 'button' && elem.type !== 'button') {
+        elementsForm.push(elem);//пушим в массив только наши инпуты
+      }
+    }
+
+    elementsForm.forEach(elem => {
+      const patternPhone = /^\+?[78]([-()]*\d){10}$/;
+      const patternText = (/^[а-яё\s]+$/i);
+      //const patternEmail = /^[\w-]+@\w+\.\w{1,}\D$/;//после точки больше 1 символа, не цифры
+
+
+      if (elem.value.trim() === '' || elem.name === 'user_phone' && !patternPhone.test(elem.value) ||
+        elem.name === 'user_name' && !patternText.test(elem.value)) {//если не проходит валидацию
+        elem.style.border = 'solid red';
+        error.add(elem);//добавл. инпуты с ошибками в Set
+        event.preventDefault();
+      } else {
+        error.delete(elem);//удал. инпуты из Seta
+        elem.style.border = '';
+      }
+
+    });
+    if (!error.size) {//если size не содержит ошибки (в Set);size коли-во эл. в массиве Set
+      getForm(event, form);
+      inputReset(form);
+    }
+  }
+
+  mainForm.addEventListener('submit', (event) => {
+    valid(event, mainForm);
+  });
+
+  captureForm.addEventListener('submit', (event) => {
+    valid(event, captureForm);
+  });
+
+  const postData = (body) => {//ф. отправки запроса
+    return fetch('./server.php', {//отправка запроса на сервер с по-ю промисов
+      method: 'POST',//отправляем и получаем
+      headers: {//заголовки
+        'Content-Type': 'application/json'//сообщаем серверу, что передаём json
+      },
+      body: JSON.stringify(body),//преобр. данные (~body) в json(строка) и передаём
+      credentials: 'include',
+      cache: 'default'
+    });
+
+  };
+
+
+};
+
+sendForm();
