5b9478987ef2964049603a9128ec41b2f83bef25
Nikolay <nikolka-net@mail.ru>
2019-12-07 15:30:09 +0600

    При нажатии на кнопку "больше.." подгружаются дополнительные блоки, сама кнопка исчезает

--- js/main.js
+++ js/main.js
@@ -267,5 +267,25 @@ const accordionTwo = () => {
 };
 accordionTwo();
 
+//More blocks, sentence
+
+const moreBlocks = () => {
+  const shadowBlocks = document.querySelectorAll(`.col-xs-12.col-sm-6.col-md-4`),
+    btnAddSentence = document.querySelector('.add-sentence-btn');
 
 
+  btnAddSentence.addEventListener('click', (event) => {
+    event.preventDefault();
+    shadowBlocks.forEach((elem) => {
+      if (elem.matches('.visible-sm-block') || elem.matches('.hidden')) {
+        elem.classList.remove('visible-sm-block', 'hidden');
+        btnAddSentence.style.cssText = `transform: scale(0)`;
+      }
+
+    });
+
+  });
+
+};
+
+moreBlocks();
