041ebc83f0cb9d36b2da0cf8ca04da5325d7e0c0
Nikolay <nikolka-net@mail.ru>
2019-12-10 01:21:20 +0600

    Собрал проект через webpack

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ elementClosest(window);//вызываем его
 import 'formdata-polyfill';
 import 'es6-promise';
 import 'fetch-polyfill';
+
+
+import modalWindow from './modules/modalWindow';
+import accordionTwo from './modules/accordionTwo';
+import moreBlocks from './modules/moreBlocks';
+import accordion from './modules/accordion';
+import sendForm from './modules/sendForm';
+
+
+modalWindow();
+
+accordionTwo();
+
+moreBlocks();
+
+accordion();
+
+sendForm();
